f6d62c53dd925415f89863c178670610ceb36ec9
ojeremias <68084107+ojeremias@users.noreply.github.com>
2020-07-09 18:22:08 -0300

    Calculadora do jeremaia

---
+++ AT001.py
@@ -0,0 +1,14 @@
+n1 = int (input('Digite um valor:'))
+n2 = int (input('Digite mais um aí, meu rei:'))
+s = n1 + n2
+print('A soma do valor é:', s)
+s = n1 / n2
+print('o resultado da divisão é:',s)
+s = n1 * n2
+print('A multiplicação é:',s)
+s = n1 ^ n2
+print('A potência é:',s)
+s = n1 % n2
+print('A porcentagem é:',s)
+
+print('Um big beijo do pai Jeremias, seus lindos')
